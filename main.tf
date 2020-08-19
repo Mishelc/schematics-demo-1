135627dc2c80938775000e289632d2f72bbb12cb
Mishel Carrion <mcarrionl@uni.pe>
2020-08-19 10:54:25 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,136 @@
+data "ibm_resource_group" "resource_group" {
+  name = "${var.resource_group}"
+}
+
+resource "ibm_is_ssh_key" "vpc-mishel" {
+  name       = "vpc-mishel"
+  # public_key = "${var.ssh_public_key}"
+}
+# Create VPC
+resource "ibm_is_vpc" "vpc1" {
+  name = "${var.vpc_name}"
+  resource_group  = "${data.ibm_resource_group.resource_group.id}"
+}
+# VPC Zones
+resource "ibm_is_vpc_address_prefix" "vpc-ap1" {
+  name = "vpc-ap1"
+  zone = "${var.zone1}"
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  cidr = "${var.zone1_cidr}"
+}
+
+resource "ibm_is_vpc_address_prefix" "vpc-ap2" {
+  name = "vpc-ap2"
+  zone = "${var.zone2}"
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  cidr = "${var.zone2_cidr}"
+}
+# Public gateway zone 1
+resource "ibm_is_public_gateway" "gateway1" {
+    name = "gateway1"
+    vpc = "${ibm_is_vpc.vpc1.id}"
+    zone = "${var.zone1}"
+}
+
+# Public gateway zone 2
+resource "ibm_is_public_gateway" "gateway2" {
+    name = "gateway2"
+    vpc = "${ibm_is_vpc.vpc1.id}"
+    zone = "${var.zone2}"
+}
+
+# Subnets Zone 1
+resource "ibm_is_subnet" "app1" {
+  name            = "app1"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone1}"
+  ipv4_cidr_block = "${var.zone1_subnet1}"
+  public_gateway = "${ibm_is_public_gateway.gateway1.id}"
+  depends_on      = ["ibm_is_vpc_address_prefix.vpc-ap1"]
+}
+
+resource "ibm_is_subnet" "bd1" {
+  name            = "bd1"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone1}"
+  ipv4_cidr_block = "${var.zone1_subnet2}"
+  depends_on      = ["ibm_is_vpc_address_prefix.vpc-ap1"]
+}
+
+# Subnets Zone 2
+resource "ibm_is_subnet" "app2" {
+  name            = "app2"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone2}"
+  ipv4_cidr_block = "${var.zone2_subnet1}"
+  public_gateway = "${ibm_is_public_gateway.gateway2.id}"
+  depends_on      = ["ibm_is_vpc_address_prefix.vpc-ap2"]
+}
+
+resource "ibm_is_subnet" "bd2" {
+  name            = "bd2"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone2}"
+  ipv4_cidr_block = "${var.zone2_subnet2}"
+  depends_on      = ["ibm_is_vpc_address_prefix.vpc-ap2"]
+}
+#App Instance Subnet 1 Zone 1
+resource "ibm_is_instance" "appinstance1" {
+  name    = "appinstance1"
+  image   = "${var.image}"
+  profile = "${var.profile}"
+
+  primary_network_interface = {
+    subnet = "${ibm_is_subnet.app1.id}"
+  }
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  zone = "${var.zone1}"
+  keys = ["${ibm_is_ssh_key.ssh1.id}"]
+  user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
+}
+# App Instance Subnet 1 Zone 2
+resource "ibm_is_instance" "appinstance2" {
+  name    = "appinstance2"
+  image   = "${var.image}"
+  profile = "${var.profile}"
+
+  primary_network_interface = {
+    subnet = "${ibm_is_subnet.app2.id}"
+  }
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  zone = "${var.zone2}"
+  keys = ["${ibm_is_ssh_key.ssh1.id}"]
+  user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
+}
+
+resource "ibm_is_floating_ip" "floatingip1" {
+  name = "fip1"
+  target = "${ibm_is_instance.appinstance1.primary_network_interface.0.id}"
+}
+
+resource "ibm_is_floating_ip" "floatingip2" {
+  name = "fip2"
+  target = "${ibm_is_instance.appinstance2.primary_network_interface.0.id}"
+}
+
+resource "ibm_is_security_group_rule" "sg1_tcp_rule_22" {
+  depends_on = ["ibm_is_floating_ip.floatingip1", "ibm_is_floating_ip.floatingip2"]
+  group     = "${ibm_is_vpc.vpc1.default_security_group}"
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  tcp = {
+    port_min = "22"
+    port_max = "22"
+  }
+}
+
+resource "ibm_is_security_group_rule" "sg1_tcp_rule_80" {
+  depends_on = ["ibm_is_floating_ip.floatingip1", "ibm_is_floating_ip.floatingip2"]
+  group     = "${ibm_is_vpc.vpc1.default_security_group}"
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  tcp = {
+    port_min = "80"
+    port_max = "80"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,71 @@
+variable "resource_group" {
+  default = "VPC"
+  description = "Name of resource group to provision resources"
+}
+
+variable "ibmcloud_region" {
+  description = "Preferred IBM Cloud region to use for your infrastructure"
+  default = "us-south"
+}
+
+variable "vpc_name" {
+  default = "vpc-demo-1"
+  description = "Name of your VPC"
+}
+
+variable "zone1" {
+  default = "us-south-1"
+  description = "Define the 1st zone of the region"
+}
+
+variable "zone2" {
+  default = "us-south-2"
+  description = "Define the 2nd zone of the region"
+}
+
+variable "zone1_cidr" {
+  default = "172.16.1.0/24"
+  description = "CIDR block to be used for zone 1"
+}
+
+variable "zone2_cidr" {
+  default = "172.16.2.0/24"
+  description = "CIDR block to be used for zone 2"
+}
+
+variable "zone1_subnet1" {
+    default = "172.16.1.0/26"
+    description = "CIDR block to be used for subnet 1 in zone 1"
+}
+
+variable "zone1_subnet2" {
+    default = "172.16.1.64/26"
+    description = "CIDR block to be used for subnet 2 in zone 1"
+}
+
+variable "zone2_subnet1" {
+    default = "172.16.2.0/26"
+    description = "CIDR block to be used for subnet 1 in zone 2"
+}
+
+variable "zone2_subnet2" {
+    default = "172.16.2.64/26"
+    description = "CIDR block to be used for subnet 1 in zone 2"
+}
+
+/*
+variable "ssh_public_key" {
+  default = "vpc-mishel"
+  description = "SSH Public Key contents to be used"
+}
+*/
+
+variable "image" {
+  default = "r006-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
+  description = "OS Image ID - ubuntu-18-04-amd64 -to be used for virtual instances"
+}
+
+variable "profile" {
+  default = "cx2-2x4"
+  description = "Instance profile to be used for virtual instances"
+}
